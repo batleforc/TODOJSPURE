8c681f74eb695f6813dfed80ea274011db532ee2
Leriche maxime <maxleriche.60@gmail.com>
2020-09-11 11:18:00 +0200

    add htmlwebpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
 module.exports = {
   entry: {
@@ -9,9 +14,7 @@ module.exports = {
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Production',
-    }),
+    HtmlWebpackPluginConfig
   ],
   output: {
     filename: '[name].bundle.js',
